8270692712c1792f980972731c32661e29124ef1
barbenezra <bar@altahq.com>
2024-08-17 10:46:56 +0300

    m

--- Assets/move.cs
+++ Assets/move.cs
@@ -5,7 +5,7 @@ public class move : MonoBehaviour
 {
     void Start()
     {
-        Vector3 target = new Vector3(transform.position.x + 10f, transform.position.y,
+        var target = new Vector3(transform.position.x + 10f, transform.position.y,
             transform.position.z);
 
         transform.DOMove(target, 3f).SetEase(Ease.InOutQuad).SetLoops(-1, LoopType.Yoyo);
